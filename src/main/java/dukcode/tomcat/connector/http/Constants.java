7d4a12bd45fdfc31bb3e5602397f40a0d098ff0f
dukcode <duk9741@gmail.com>
2022-09-15 18:10:11 +0900

    feat: SocketInputStream, HttpRqeustLine, HttpHeader 가져옴

--- src/main/java/dukcode/tomcat/connector/http/Constants.java
+++ src/main/java/dukcode/tomcat/connector/http/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final int BASIC_PORT = 8080;
     public static final String WEB_ROOT =
             System.getProperty("user.dir") + File.separator + "webroot";
+    public static final String Package = "dukcode.tomcat.connector.http";
 }
